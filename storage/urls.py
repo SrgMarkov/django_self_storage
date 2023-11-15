55d757df2319ed8f2623008867ac4821ead7b272
frqhero <frqhero@gmail.com>
2023-11-15 15:02:03 +0600

    Connect faq & boxes pages to index page

--- storage/urls.py
+++ storage/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'posts'
+app_name = 'storage'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('boxes/', views.boxes, name='boxes'),
+    path('faq/', views.faq, name='faq'),
 ]
--- storage/views.py
+++ storage/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'storage/index.html')
+
+
+def boxes(request):
+    return render(request, 'storage/boxes.html')
+
+
+def faq(request):
+    return render(request, 'storage/faq.html')
