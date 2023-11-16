790568256ff204843152714a31fd158672b808f9
zatomis <zatomis@list.ru>
2023-11-16 13:11:42 +0300

    Модели

--- backend/storage/models.py
+++ backend/storage/models.py
@@ -1,3 +1,66 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+import qrcode
+from datetime import datetime
+import datetime, calendar
 
-# Create your models here.
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year, month)[1])
+    return datetime.date(year, month, day)
+
+
+class Stock(models.Model):
+    address = models.CharField(max_length=400, verbose_name='Адрес склада')
+    property = models.CharField(max_length=400, verbose_name='Характеристика, свойства склада')
+    capacity = models.PositiveBigIntegerField(null=True, default=0)
+
+    def __str__(self):
+        return f'{self.address} - {self.property} - {self.capacity}'
+
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
+
+class BoxX(models.Model):
+    """
+        по номеру бокса - можно потом QR сделать...
+    """
+    # picture = models.ImageField(blank=True, verbose_name='Изображение')
+    box_number = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='Номер бокса')
+    capacity = models.PositiveBigIntegerField(null=True, default=0)
+    boxx = models.ForeignKey(Stock, related_name='stock_box', verbose_name='Склад', blank=True,  on_delete=models.CASCADE)
+    # create_date = models.DateTimeField(auto_now_add=True, blank=True, null=True, default=datetime.datetime.now())
+    create_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    involved = models.BooleanField(default=False, verbose_name='Задействован')
+    box_qr_code = models.ImageField(upload_to='images_qr')
+
+    def save(self, *args, **kwargs):
+        img = qrcode.make(self.box_number)
+        box_qr_code = img.save(f"images_qr/{self.box_number}.png")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'№{self.box_number} дата создания {self.create_date}'
+
+    class Meta:
+        verbose_name = 'Бокс'
+        verbose_name_plural = 'Боксы'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    boxx = models.ForeignKey(BoxX, related_name='user_box', verbose_name='ячейка', blank=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=400, verbose_name='Адрес клиента')
+
+    def __str__(self):
+        return f'{self.user} {self.address}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
