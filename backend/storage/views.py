325fb1ea9ab0d9195fbe8c624e37c830517f5871
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-11-17 11:32:31 +0400

    Add registration

--- backend/storage/views.py
+++ backend/storage/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from .forms import LoginUserForm, RegisterUserForm
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'registration.html' # link to registration form
+    success_url = reverse_lazy('auth')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'auth.html' # link to authorization form
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('lk') #  link to user page
+
+
+def logout_user(request):
+    logout(request)
+    request.session.clear()
+    return redirect('index')
+
+
+def lk(request):
+    return render(request, 'storage/lk.html')
 
 
 def index(request):
